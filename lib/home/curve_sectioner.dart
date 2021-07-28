032a613c293a491bf9410d36ec192f5a62787ee4
neondyis <452711@student.saxion.nl>
2021-07-28 14:13:56 +0200

    fixed the row margin and text to button

--- lib/home/curve_sectioner.dart
+++ lib/home/curve_sectioner.dart
@@ -1,14 +1,25 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class CurvePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint();
 
-    paint.color = Colors.lightBlue;
-    paint.style = PaintingStyle.stroke;
     paint.strokeWidth = 3;
+    paint
+      ..shader = ui.Gradient.linear(
+        Offset(size.width * 0.2, size.height),
+        Offset(size.width, size.height),
+        [
+          Color(0xFFa1c4fd),
+          Color(0xFFc2e9fb),
+        ],
+      );
+    paint..isAntiAlias = true;
 
     var startPoint = Offset(0, size.height);
     var controlPoint1 = Offset(size.width / 2, size.height / 2);
@@ -17,10 +28,21 @@ class CurvePainter extends CustomPainter {
 
     var path = Path();
 
-    path.moveTo(startPoint.dx, startPoint.dy);
-    path.cubicTo(controlPoint1.dx, controlPoint1.dy, controlPoint2.dx,
-        controlPoint2.dy, endPoint.dx, endPoint.dy);
+    path.lineTo(0, size.height);
+    // path.moveTo(startPoint.dx, startPoint.dy);
+    // path.cubicTo(controlPoint1.dx, controlPoint1.dy, controlPoint2.dx,
+    //     controlPoint2.dy, endPoint.dx, endPoint.dy);
 
+    path.quadraticBezierTo(
+        size.width * 0.30, size.height * 1, size.width * 0.40, size.height * 1);
+
+    path.quadraticBezierTo(size.width * 0.6, size.height * 1, size.width * 0.70,
+        size.height * 1.20);
+
+    path.quadraticBezierTo(
+        size.width * 1, size.height * 1.8, size.width * 1.2, 0);
+
+    path.close();
     canvas.drawPath(path, paint);
   }
 
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -16,29 +16,7 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
   int _selectedIndex = 0;
   WeatherFactory wf = new WeatherFactory('3bcedd8fd983c097abd4899a78961b7f',
       language: Language.ENGLISH);
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<String> _cityNames = ['Deventer', 'Apeldoorn', 'Enschede'];
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,7 +48,7 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
           ),
         ),
         body: Padding(
-            padding: const EdgeInsets.only(top: 5.0),
+            padding: const EdgeInsets.only(top: 0.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
@@ -81,8 +59,6 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
                       Expanded(
                         flex: 4,
                         child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.width * 0.5,
                           child: Row(
                             children: <Widget>[CurvedWidget()],
                           ),
@@ -95,21 +71,24 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
                             child: Column(
                               children: [
                                 Row(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Container(
-                                      margin: new EdgeInsets.symmetric(
-                                          horizontal: 60.0, vertical: 13.0),
-                                      child: TextButton(
-                                          onPressed: () => {},
-                                          style: ElevatedButton.styleFrom(
-                                              primary: Colors.red,
-                                              onPrimary: Colors.white,
-                                              shape: CircleBorder(),
-                                              padding: EdgeInsets.all(24)),
-                                          child: Text('lol')),
+                                    Expanded(
+                                      flex: 4,
+                                      child: Text(
+                                        "Amsterdam",
+                                        textAlign: TextAlign.center,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0,
+                                          color: Colors.blue[700],
+                                        ),
+                                      ),
                                     ),
+                                    Expanded(flex: 2, child: HomeBottom()),
                                   ],
                                 ),
                               ],
@@ -122,6 +101,43 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
             )),
       ),
     );
+  }
+}
+
+class HomeBottom extends StatelessWidget {
+  const HomeBottom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+          // margin: new EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+          child: CurveButton()),
+    );
+  }
+}
+
+class CurveButton extends StatelessWidget {
+  const CurveButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () => {},
+        style: ElevatedButton.styleFrom(
+            elevation: 0,
+            primary: Colors.white,
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(24)),
+        child: IconButton(
+          icon: const Icon(Icons.add),
+          color: Colors.black.withOpacity(0.5),
+          onPressed: () {},
+        ));
   }
 }
 
